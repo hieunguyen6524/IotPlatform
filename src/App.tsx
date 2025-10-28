4965131c37d5452b49a35243b17adbc5809bb35d
Hieu Nguyen <hieunguyen6524@gmail.com>
2025-10-28 23:35:58 +0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 // import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
-import Dashboard from "./pages/DashBoard_v2";
+import Dashboard from "./pages/DashBoard_v3";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return <Dashboard />;
 }
 
